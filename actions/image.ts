27fa3f8a7794c1272590f58deb19f59a79402ba7
anomsejogatihb <anom.sejogati@healthbeats.co>
2025-01-02 09:04:26 +0700

    Fix getImageFromDB

--- actions/image.ts
+++ actions/image.ts
@@ -130,7 +130,8 @@ export const getImageFromDB = async (_id: string) => {
   try {
     await db();
 
-    const image = await Image.findById(_id);
+    const [image] = await Promise.all([Image.findById(_id)]);
+
     return JSON.parse(JSON.stringify(image));
   } catch (error: any) {
     throw new Error(error);
